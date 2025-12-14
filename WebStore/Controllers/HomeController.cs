f1160d636f31e95d5c4296ddedae4ef92615132e
Тимофей Приходько <t232006@gmail.com>
2025-12-14 20:13:11 +0500

    Список сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, Name = "Alice", Position = "Developer" , DateOfBirth=new DateTime(2002,12,12) },
+            new Employee { Id = 2, Name = "Bob", Position = "Designer" , DateOfBirth=new DateTime(2001,11,11) },
+            new Employee { Id = 3, Name = "Charlie", Position = "Manager", DateOfBirth=new DateTime(2000,10,10) }
+        };
         public IActionResult Index()
         {
             return View();
@@ -11,6 +18,10 @@ namespace WebStore.Controllers
         public IActionResult HelloID(string? ID)
         {
             return Content($"Hello from ID -{ID}");
+        }
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Age { get => Convert.ToInt32((DateTime.Today - DateOfBirth).TotalDays/365); }
+    }
+}
