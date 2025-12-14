1fc83e4b5cf278990cbfa2725256c1d19498525b
Тимофей Приходько <t232006@gmail.com>
2025-12-14 22:01:22 +0500

    qr codes

---
+++ WebStore/Controllers/QRController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace WebStore.Controllers
+{
+    public class QRController: Controller
+    {
+        public IActionResult Code(string code)
+        {
+            var Generator = new QRCodeGenerator();
+            var Data = Generator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            var QRCode = new PngByteQRCode(Data);
+            var image_bytes = QRCode.GetGraphic(20);
+            return File(image_bytes, "image/png");
+        } 
+        
+    }
+}
--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -3,8 +3,8 @@
     public class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Position { get; set; }
+        public string? Name { get; set; }
+        public string? Position { get; set; }
         public DateTime DateOfBirth { get; set; }
         public int Age { get => Convert.ToInt32((DateTime.Today - DateOfBirth).TotalDays/365); }
     }
