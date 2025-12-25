b7d474c1611ea0c8b98baeb565bef7269073accd
Тимофей Приходько <t232006@gmail.com>
2025-12-25 17:36:22 +0100

    Interfaces

---
+++ WebStore/Servises/Interfaces/IStaffData.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Servises.Interfaces
+{
+    public interface IStaffData
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? GetByID(int Id);
+        bool Edit(Employee empl);
+        int Insert(Employee empl);
+        bool Delete(int Id);
+    }
+}
