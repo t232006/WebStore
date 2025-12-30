50806b0510efef957689a7df653a957f8b89a87c
Тимофей Приходько <t232006@gmail.com>
2025-12-30 22:55:14 +0100

    Brand and Section determination

---
+++ WebStore.Domain/Base/Section.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.Base.Interfaces;
+
+namespace WebStore.Domain.Base
+{
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        int? ParentID { get; set; }
+    }
+
+
+}
