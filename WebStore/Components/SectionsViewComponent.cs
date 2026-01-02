a7fc85d22af3a982caf8d4a8ad8f3c1e6ec987eb
Тимофей Приходько <t232006@gmail.com>
2026-01-02 22:38:09 +0100

    SectionViewComponent complete

--- WebStore/Components/SectionsViewComponent.cs
+++ WebStore/Components/SectionsViewComponent.cs
@@ -1,9 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Servises;
+using WebStore.Servises.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Components
 {
     public class SectionsViewComponent : ViewComponent
     {
-       public IViewComponentResult Invoke() => View();
+        InMemoryProductData msd;
+        public SectionsViewComponent(InMemoryProductData MSD) => msd = MSD;
+
+        public IViewComponentResult Invoke()
+        {
+            var sections = msd.GetSections();
+            var parent_section = sections.Where(s => s.ParentID is null).OrderBy(s => s.Order);
+            var parent_section_views=parent_section.Select(s => new SectionsViewModel
+            {
+                ID=s.ID,
+                Name=s.Name
+            }).ToArray();
+            foreach(var psv in parent_section_views)
+            {
+                var childs = sections.Where(s => s.ParentID == psv.ID).OrderBy(s => s.Order);
+                foreach(var ch in childs)
+                {
+                    psv.ChildSection.Add(new SectionsViewModel
+                    {
+                        ID = ch.ID,
+                        Name = ch.Name
+                    });
+                }
+            }
+            return View(parent_section_views);
+        }
     }
 }
