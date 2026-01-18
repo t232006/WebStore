0488f61cb8d11a60994e08e012f1889b805b502a
Тимофей Приходько <t232006@gmail.com>
2026-01-18 22:55:13 +0100

    DB Context

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Base;
+
+namespace WebStore.DAL.Context
+{
+    internal class WebStoreDB :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public WebStoreDB (DbContextOptions<WebStoreDB> DBOptions) : base(DBOptions)
+        {
+
+        }
+
+    }
+}
