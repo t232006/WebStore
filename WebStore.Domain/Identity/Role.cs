1ba28803d9725bff26218d614c7f969a2f6a6072
Тимофей Приходько <t232006@gmail.com>
2026-01-26 11:22:14 +0100

    User and Role determination

---
+++ WebStore.Domain/Identity/Role.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Identity
+{
+    public class Role: IdentityRole
+    {
+
+    }
+
+}
---
+++ WebStore.Domain/Identity/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Domain.Base;
+
+namespace WebStore.Domain.Identity
+{
+    public class User: IdentityUser
+    {
+        public DateTime regData { get; set; }
+        ICollection<Blog> Blogs { get; set; } = new HashSet<Blog>();
+    }
+
+}
