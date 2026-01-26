bf7aa3b7875276fc231fde774560e9f14547a86f
Тимофей Приходько <t232006@gmail.com>
2026-01-26 13:04:18 +0100

    AccountController

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController :Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signinManager;
+        private readonly Logger<AccountController> logger;
+
+        public AccountController(
+            UserManager<User> _userManager, 
+            SignInManager<User> _signinManager,
+            Logger<AccountController> logger)
+        {
+            userManager = _userManager;
+            signinManager = _signinManager;
+            this.logger = logger;
+        }
+        IActionResult Register() => View();
+        IActionResult Login() => View();
+        IActionResult Logout() => View();
+        IActionResult AccessDenied() => View();
+
+
+    }
+}
