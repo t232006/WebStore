b36e67d52332173e1747ce82643e1a8a6a62ba96
Тимофей Приходько <t232006@gmail.com>
2026-02-10 23:55:18 +0100

    StarsRate component completed

--- WebStore/Services/InSQL/InSQLBlogUserRate.cs
+++ WebStore/Services/InSQL/InSQLBlogUserRate.cs
@@ -14,12 +14,15 @@ namespace WebStore.Services.InSQL
         }
         public decimal AverageRate(int BlogID)
         {
-            return db.BlogUserRate.Where(br => br.BlogID == BlogID).Average(r => r.Rate);
+            var RatesList = db.BlogUserRate.Where(br => br.BlogID == BlogID);
+            if (RatesList.Any())
+                return RatesList.Average(r => r.Rate);
+            return 0;
         }
 
         public void Invote(string UserID, int BlogID, byte Rate)
         {
-            if (UserVote(UserID, BlogID) is null)
+            if (UserVote(UserID, BlogID) is null||(UserVote(UserID, BlogID)==0))
             {
                 db.BlogUserRate.Add(new Domain.Base.BlogUserRate
                 {
@@ -39,9 +42,10 @@ namespace WebStore.Services.InSQL
         public byte? UserVote(string? UserID, int BlogID)
         {
             if (UserID == "") return null;
-            var uv = db.BlogUserRate.Where(u => u.UserID == UserID).Where(b => b.BlogID == BlogID)
-                .Select(r => r.Rate).First();
-            return (byte)uv;
+            var uv = db.BlogUserRate.Where(u => u.UserID == UserID).Where(b => b.BlogID == BlogID);
+            if (uv.Any())
+            return  (byte)uv.Select(r => r.Rate).First();
+            return 0;
         }
 
         public int VoteNumber(int BlogID)
