23390c28a8f4bb200167110bc186d92bda8997e6
Тимофей Приходько <t232006@gmail.com>
2026-03-27 13:04:58 +0100

    Cart service determination

---
+++ WebStore.Domain/Base/Interfaces/ICart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Base.Interfaces
+{
+    public interface ICart
+    {
+        public void Add(int ProductID);
+        public void Decriment(int ProductID);
+        public void Remove(int ProductID);
+        public void Clear(); 
+        public
+    }
+}
---
+++ WebStore/ViewModels/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStore.ViewModels
+{
+    public class CartViewModel
+    {
+        public IEnumerable<(ProductViewModel Product, int Quantity)> Items { get; set; }
+        public decimal TotalPrice(int ProductID) => Items.Sum(pr => pr.Quantity * pr.Product.Price);
+        public int ItemsCount => Items.Sum(item => item.Quantity);
+
+    }
+}
