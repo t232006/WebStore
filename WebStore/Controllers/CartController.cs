70b4f59fbe768dfce3286e516fd3ebe7187ed8f1
Тимофей Приходько <t232006@gmail.com>
2026-03-27 23:18:24 +0100

    visual part of Cart

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Base.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController:Controller
+    {
+        private readonly ICart _cart;
+
+        public CartController(ICart cart)
+        {
+            _cart = cart;
+        }
+        public IActionResult Index() => View(_cart.GetViewModel());
+    }
+}
--- WebStore/ViewModels/CartViewModel.cs
+++ WebStore/ViewModels/CartViewModel.cs
@@ -3,7 +3,7 @@
     public class CartViewModel
     {
         public IEnumerable<(ProductViewModel Product, int Quantity)> Items { get; set; }
-        public decimal TotalPrice(int ProductID) => Items.Sum(pr => pr.Quantity * pr.Product.Price);
+        public decimal TotalPrice() => Items.Sum(pr => pr.Quantity * pr.Product.Price);
         public int ItemsCount => Items.Sum(item => item.Quantity);
 
     }
